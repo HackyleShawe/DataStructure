24a65eaa7d6db2bf56f3a07d3a735464b2508fdf
Hackyle <kyleshawe@outlook.com>
2019-11-11 21:07:20 +0800

    单链表的头插法和尾插法

---
+++ 01-LinearList/A03-链表的算法/A不带头单链表插入操作02-尾插法.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+typedef struct Node
+{
+	ElemType data;
+	struct Node* next;
+}ListNode, * LinkList; //
+
+/*尾插法
+Algorithm Core：
+	每次插入的新节点都在结尾处
+*/
+LinkList Create_Tail_Insert()
+{
+	LinkList L = NULL;
+	LinkList s, r = NULL;
+	int x;
+
+	scanf("%d", &x);
+	while (x != 99)
+	{
+		s = (ListNode*)malloc(sizeof(ListNode));
+		s->data = x;
+		if (L == NULL) //对第一个节点的处理
+		{
+			L = s;
+		}
+		else //非第一个节点的处理
+		{
+			r->next = s;
+		}
+		r = s;
+		scanf("%d", &x);
+	}
+	if (r != NULL)
+	{
+		r->next = NULL;
+	}
+	return L;
+}
+
+void show(LinkList L)
+{
+	LinkList p = L;
+	while (p)
+	{
+		printf("%d ", p->data);
+		p = p->next;
+	}
+	printf("\n");
+}
+
+void test01()
+{
+	LinkList L;
+	L = Create_Tail_Insert();
+	show(L);
+}
+int main()
+{
+	test01();
+	return 0;
+}
