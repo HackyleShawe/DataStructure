96957b1bec9fa15022b7011c11bd7ab7c9e071f1
Hackyle <kyleshawe@outlook.com>
2020-03-01 15:29:27 +0800

    插入类排序

---
+++ 02-Sort/A-插入类排序/直接插入排序.java
@@ -0,0 +1,44 @@
+
+import java.util.Random;
+import org.junit.Test;
+
+
+public class InsertSort {
+	private int n = 10;
+	
+	public void getArray(int[] a, int n) {
+		Random r = new Random();
+		for(int i=0; i<n; i++) {
+			a[i] = r.nextInt(1000);
+		}
+		showArray(a,n);
+	}
+	public void showArray(int[] a, int n) {
+		for(int i=0; i<n; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	
+	/**
+	 * 直接插入排序
+	 */
+	@Test
+	public void testInsertSort() {
+		int[] a = new int[n];
+		getArray(a,n);
+		
+		for(int i=0; i<n; i++) {
+			int t = a[i];
+			int j = 0;
+			for(j=i-1; j>=0 && t<a[j]; j--) { //找
+				a[j+1] = a[j]; //挪
+			}
+			a[j+1] = t; //插入
+		}
+		
+		showArray(a,n);
+	}
+	
+}
